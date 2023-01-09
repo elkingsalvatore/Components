bdee57a107664b984d9df105b67efea76168f991
elkingsalvatore <elkinggomez98@gmail.com>
2023-01-09 15:53:41 -0600

    input
    más widgets y propiedades

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 //import 'package:componentes/src/pages/home_temp.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:componentes/src/routes/routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 
@@ -15,6 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+      Locale('en', 'US'), // English, no country code
+      Locale('es', 'ES'), // Spanish, no country code
+  ],
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
--- lib/src/pages/input.dart
+++ lib/src/pages/input.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_final_fields, unnecessary_new, prefer_const_constructors, sort_child_properties_last
+
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 
@@ -10,7 +14,16 @@ class InputPage extends StatefulWidget {
 
 class _InputPage extends State<InputPage> {
 
-  String _nombre = ''; //Debo de iniciar mi variable vacía para que no hbay problema con el tipo late
+  String _nombre = '';
+  String _email = ''; //Debo de iniciar mi variable vacía para que no hbay problema con el tipo late
+  String _fecha = '';
+
+  String _opcionSeleccionada = 'Volar';
+
+  List <String> _poderes = ['Volar','Rayos X','Super aliento','Super fuerza'];
+
+  TextEditingController _inputFiedDateController = new TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +35,14 @@ class _InputPage extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearFecha(context),
+          Divider(),
+          _crearDropdown(),
           Divider(),
            _crearPersona()
         ],
@@ -54,17 +75,138 @@ class _InputPage extends State<InputPage> {
   }
   
 
-
-
-
-
-
-
  Widget _crearPersona() {
 
   return ListTile(
     title: Text('Nombre es: $_nombre'),
+    subtitle: Text('Email: $_email'),
+    leading: Text(_opcionSeleccionada),
   );
 
  }
+ 
+  Widget _crearEmail() {
+    return TextField(
+      //autofocus: true,
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        
+        hintText: 'Email',
+        labelText: 'Email',
+        suffixIcon: Icon( Icons.alternate_email ),
+        icon: Icon( Icons.email)
+      ),
+      onChanged: (valor){
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+  
+  Widget _crearPassword() {
+    return TextField(
+      //autofocus: true,
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        
+        hintText: 'Password',
+        labelText: 'Password',
+        suffixIcon: Icon( Icons.lock_open),
+        icon: Icon( Icons.lock)
+      ),
+      onChanged: (valor){
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+  
+ Widget _crearFecha(BuildContext context) {
+  return TextField(
+      enableInteractiveSelection: false,
+      controller: _inputFiedDateController,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        
+        hintText: 'Fecha de nacimiento',
+        labelText: 'Fecha de nacimiento',
+        suffixIcon: Icon( Icons.perm_contact_calendar),
+        icon: Icon( Icons.calendar_today)
+      ),
+      onTap: (){
+
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate( context);
+      },
+    );
+ }
+ 
+ _selectDate(BuildContext context) async{
+
+  DateTime? picked = await showDatePicker(
+    context: context, 
+    initialDate: new DateTime.now(),
+    firstDate: new DateTime(2020),
+    lastDate: new DateTime(2027),
+    locale: Locale('es', 'ES'),
+    );
+
+    if (picked != null){
+      setState(() {
+        _fecha = picked.toString();
+        _inputFiedDateController.text = _fecha;
+      });
+    }
+
+ }
+ 
+
+List<DropdownMenuItem<String>> getOpcionesDropdown(){
+
+  List<DropdownMenuItem<String>> lista = List.empty();
+
+  _poderes.forEach((poder) { 
+
+    lista.add(DropdownMenuItem(
+      child: Text(poder),
+      value: poder,
+      ));
+
+  });
+
+  return lista;
+
+}
+
+  Widget _crearDropdown() {
+
+    return Row(
+      children: <Widget>[
+        Icon(Icons.select_all),
+        SizedBox(width: 30.0),
+        Expanded(
+          child: DropdownButton(
+           value: _opcionSeleccionada,
+           items: getOpcionesDropdown(),
+           onChanged: (opt){
+             setState(() {
+                _opcionSeleccionada = opt!;
+              });
+            },
+          ),
+        )
+      ],
+    );
+    
+  }
 }
